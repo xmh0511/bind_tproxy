da5c8c62093f43d52b5de9385ea3eae8d5aba46a
xmh0511 <970252187@qq.com>
2024-04-16 14:15:50 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,11 @@ use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
+    #[cfg(target_family = "unix")]
+    const MTU: u16 = 1500;
+    #[cfg(target_os = "windows")]
+    const MTU: u16 = u16::MAX;
+
     let tun_name = "utun6";
     let (tx, mut rx) = tokio::sync::mpsc::channel::<()>(1);
     ctrlc2::set_async_handler(async move {
@@ -32,20 +37,12 @@ async fn main() {
         .destination((10, 0, 0, 9)) //Windows下不能是10.0.0.255否则无法路由全局流量
         .netmask((255, 255, 255, 0))
         .tun_name(tun_name)
-        .mtu(1500)
+        .mtu(MTU)
         .up();
     let mut stack_config = ipstack::IpStackConfig::default();
-    stack_config.mtu(1500);
+    stack_config.mtu(MTU);
     let mut ipstack =
         ipstack::IpStack::new(stack_config, tun2::create_as_async(&tun_config).unwrap());
-
-    // let args = tproxy_config::TproxyArgs::new()
-    //     .tun_name(tun_name)
-    //     .tun_mtu(1500)
-    //     .tun_ip(IpAddr::from(([10, 0, 0, 1])))
-    //     .tun_netmask(IpAddr::from(([255, 255, 255, 0])))
-	// 	.tun_gateway(IpAddr::from([10, 0, 0, 255]));
-    // let route_state = tproxy_config::tproxy_setup(&args).unwrap();
 
     #[cfg(target_family = "unix")]
     let tun_index = {
@@ -56,45 +53,43 @@ async fn main() {
 
     let handle = net_route::Handle::new().unwrap();
 
-	let outbound_index = {
-		let default = handle.default_route().await.unwrap().unwrap();
-		default.ifindex.unwrap()
-	};
+    let outbound_index = {
+        let default = handle.default_route().await.unwrap().unwrap();
+        default.ifindex.unwrap()
+    };
 
-	#[cfg(target_os = "linux")]
-	let outbound_name = {
-		let mut name = [0i8;128];
-		unsafe {
-			let ptr = name.as_mut_ptr();
-			let s = libc::if_indextoname(outbound_index, ptr);
-			let name = CStr::from_ptr(s).to_owned().to_str().unwrap().to_owned();
-			name
-		}
-	};
+    #[cfg(target_os = "linux")]
+    let outbound_name = {
+        let mut name = [0i8; 128];
+        unsafe {
+            let ptr = name.as_mut_ptr();
+            let s = libc::if_indextoname(outbound_index, ptr);
+            let name = CStr::from_ptr(s).to_owned().to_str().unwrap().to_owned();
+            name
+        }
+    };
 
-	//handle.delete(&Route::new(IpAddr::from([10, 0, 0, 0]), 24).with_ifindex(tun_index).with_gateway(IpAddr::from([10,0,0,255]))).await;
-	#[cfg(target_os = "linux")]
-	let routes = [
-		Route::new(IpAddr::from([0, 0, 0, 0]), 1).with_ifindex(tun_index),  // does not work on macOS
+    #[cfg(target_os = "linux")]
+    let routes = [
+        Route::new(IpAddr::from([0, 0, 0, 0]), 1).with_ifindex(tun_index), // does not work on macOS
         Route::new(IpAddr::from([128, 0, 0, 0]), 1).with_ifindex(tun_index),
     ];
 
-	#[cfg(target_os = "macos")]
+    #[cfg(target_os = "macos")]
     let routes = [
-		// Route::new(IpAddr::from([0, 0, 0, 0]), 1).with_ifindex(tun_index),  // does not work on macOS
+        // Route::new(IpAddr::from([0, 0, 0, 0]), 1).with_ifindex(tun_index),  // does not work on macOS for bind_device
         Route::new(IpAddr::from([1, 0, 0, 0]), 8).with_ifindex(tun_index),
-		Route::new(IpAddr::from([2, 0, 0, 0]), 7).with_ifindex(tun_index),
-		Route::new(IpAddr::from([4, 0, 0, 0]), 6).with_ifindex(tun_index),
-		Route::new(IpAddr::from([8, 0, 0, 0]), 5).with_ifindex(tun_index),
-		Route::new(IpAddr::from([16, 0, 0, 0]), 4).with_ifindex(tun_index),
-		Route::new(IpAddr::from([32, 0, 0, 0]), 3).with_ifindex(tun_index),
-		Route::new(IpAddr::from([64, 0, 0, 0]), 2).with_ifindex(tun_index),
+        Route::new(IpAddr::from([2, 0, 0, 0]), 7).with_ifindex(tun_index),
+        Route::new(IpAddr::from([4, 0, 0, 0]), 6).with_ifindex(tun_index),
+        Route::new(IpAddr::from([8, 0, 0, 0]), 5).with_ifindex(tun_index),
+        Route::new(IpAddr::from([16, 0, 0, 0]), 4).with_ifindex(tun_index),
+        Route::new(IpAddr::from([32, 0, 0, 0]), 3).with_ifindex(tun_index),
+        Route::new(IpAddr::from([64, 0, 0, 0]), 2).with_ifindex(tun_index),
         Route::new(IpAddr::from([128, 0, 0, 0]), 1).with_ifindex(tun_index),
     ];
 
     #[cfg(target_os = "windows")]
-    let routes = [
-    ];
+    let routes = [];
 
     for r in &routes {
         handle.add(r).await.unwrap();
@@ -104,8 +99,9 @@ async fn main() {
         while let Ok(stream) = ipstack.accept().await {
             match stream {
                 ipstack::stream::IpStackStream::Tcp(tcp) => {
-                    let comming_str = format!("src {} -> dest {}", tcp.local_addr(), tcp.peer_addr());
-                    println!("{}",comming_str);
+                    let comming_str =
+                        format!("src {} -> dest {}", tcp.local_addr(), tcp.peer_addr());
+                    println!("{}", comming_str);
                     // if tcp.peer_addr().ip().to_string() == "101.35.230.139" {
                     //     panic!("loop routing");
                     // }
@@ -121,14 +117,16 @@ async fn main() {
                         //     NonZeroU32::new(unsafe { libc::if_nametoindex(out_name.as_ptr()) })
                         // };
                         // socket.bind_device_by_index_v6(index).unwrap();
-                        socket.bind_device_by_index_v4(Some(NonZeroU32::new(outbound_index)).unwrap()).unwrap();
-						// unsafe {
-						// 	let index = index.unwrap().get();
-						// 	if libc::setsockopt(socket.as_fd().as_raw_fd(), libc::IPPROTO_IP, libc::IP_BOUND_IF, std::ptr::addr_of!(index).cast(), std::mem::size_of::<u32>() as libc::socklen_t) == -1{
-						// 		panic!("setsockopt error");
-						// 	}
-						// };
-						//socket.bind(&socket2::SockAddr::from(SocketAddr::from(([192,168,1,1],0))));
+                        socket
+                            .bind_device_by_index_v4(Some(NonZeroU32::new(outbound_index)).unwrap())
+                            .unwrap();
+                        // unsafe {
+                        // 	let index = index.unwrap().get();
+                        // 	if libc::setsockopt(socket.as_fd().as_raw_fd(), libc::IPPROTO_IP, libc::IP_BOUND_IF, std::ptr::addr_of!(index).cast(), std::mem::size_of::<u32>() as libc::socklen_t) == -1{
+                        // 		panic!("setsockopt error");
+                        // 	}
+                        // };
+                        //socket.bind(&socket2::SockAddr::from(SocketAddr::from(([192,168,1,1],0))));
                     }
                     #[cfg(target_os = "windows")]
                     {
@@ -136,9 +134,14 @@ async fn main() {
                         //use windows::Win32::Networking::WinSock::SOCKET;
                         let sock = WinSock::SOCKET(socket.as_socket().as_raw_socket() as usize);
                         let big = outbound_index.to_be_bytes();
-                        unsafe{
-                            let r = WinSock::setsockopt(sock, WinSock::IPPROTO_IP.0, WinSock::IP_UNICAST_IF, Some(&big));
-                            if r !=0{
+                        unsafe {
+                            let r = WinSock::setsockopt(
+                                sock,
+                                WinSock::IPPROTO_IP.0,
+                                WinSock::IP_UNICAST_IF,
+                                Some(&big),
+                            );
+                            if r != 0 {
                                 panic!("setsockopt r = {r}");
                             }
                         }
@@ -150,10 +153,17 @@ async fn main() {
                         ))))
                         .unwrap();
                     socket.set_nonblocking(true).unwrap();
-                    let timeout = std::time::Duration::from_secs(5);
+                    //let timeout = std::time::Duration::from_secs(5);
                     tokio::spawn(async move {
-                        my_bidirection_copy(tcp, tokio::net::TcpStream::from_std(socket.into()).unwrap(),timeout,comming_str)
-                            .await;
+                        let mut socket = tokio::net::TcpStream::from_std(socket.into()).unwrap();
+                        //my_bidirection_copy(tcp, socket, timeout, comming_str).await;
+                        let mut tcp = tcp;
+                        match tokio::io::copy_bidirectional(&mut tcp, &mut socket).await {
+                            Ok(_v) => {}
+                            Err(e) => {
+                                println!("{comming_str} {e:?}");
+                            }
+                        };
                     });
                 }
                 ipstack::stream::IpStackStream::Udp(_) => {}
@@ -162,8 +172,8 @@ async fn main() {
             }
         }
     });
+    println!("Starting!!!!!");
     rx.recv().await.expect("Could not receive from channel.");
-    // tproxy_config::tproxy_remove(Some(route_state)).unwrap();
 
     for r in &routes {
         handle.delete(r).await.unwrap();
@@ -172,7 +182,8 @@ async fn main() {
     println!("Got it! Exiting...");
 }
 
-async fn my_bidirection_copy<L, R>(lhs: L, rhs: R,timeout:std::time::Duration,info:String)
+#[allow(dead_code)]
+async fn my_bidirection_copy<L, R>(lhs: L, rhs: R, timeout: std::time::Duration, info: String)
 where
     L: AsyncRead + AsyncWrite + Send + Sync + 'static,
     R: AsyncRead + AsyncWrite + Send + Sync + 'static,
@@ -208,7 +219,7 @@ where
         #[allow(unreachable_code)]
         Ok(())
     });
-    while let Some(_v) = join_set.join_next().await{
+    while let Some(_v) = join_set.join_next().await {
         //println!("join await {v:?}");
     }
     println!("====== end tcp connection {info} ======");
